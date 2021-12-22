a2da624eedec2f16f292b83ba28aefef1fa90879
NS-LE <96488124+NS-LE@users.noreply.github.com>
2021-12-22 12:22:09 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,32 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+const fetch = require('node-fetch')
+
+const API_ENDPOINT = 'https://api.contentful.com/spaces/bhxah9dfjc2u/environments/master/entries/6pX6vujnVHCaEONrSqflyU?access_token=CFPAT-On1cwLZn2kUACkRW0BwL9om4GQAaPsoQcClUjIgkqDU'
+
+exports.handler = async (event, context) => {
+  let response
+  try {
+    response = await fetch(API_ENDPOINT)
+    // handle response
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response
+    })
+  }
+}
+
 export default function Home() {
   return (
     <div className="container">
